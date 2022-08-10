8b4823f0ea7d6362731847575fea72d9131dde03
lsjpjs1 <lsjpjs1@naver.com>
2022-08-11 03:20:34 +0900

    feat: google analytics 초기 설정

--- src/index.tsx
+++ src/index.tsx
@@ -9,7 +9,11 @@ import {Provider} from "react-redux";
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import MainContainer from "./containers/MainContainer";
 import ServerMaintenanceContainer from "./containers/ServerMaintenanceContainer";
+import ReactGA from 'react-ga4'; // GA4
 
+
+const TRACKING_ID = process.env["REACT_APP_GOOGLE_ANALYTICS_TRACKING_ID"]; // 발급받은 추적ID를 환경 변수로 불러온다.
+ReactGA.initialize(TRACKING_ID); //new
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
