97ff66e2bafb652410f36d38d3305ad758bba2b7
lsjpjs1 <lsjpjs1@naver.com>
2022-10-11 02:00:09 +0900

    feat: 공유하기 기능 추가

--- src/components/HotDealListView.tsx
+++ src/components/HotDealListView.tsx
@@ -72,12 +72,22 @@ const HotDealListView = (props: Props) => {
                         </a>
                 }
                 <Button onClick={async (e)=>{
-                    try {
-                        await navigator.clipboard.writeText(`https://whendiscount.com/hot-deals/${hotDeal.hotDealId}`);
-                        alert('클립보드에 복사되었습니다.');
-                    } catch (error) {
-                        alert('복사에 실패하였습니다.');
-                    }
+                    // try {
+                    //     await navigator.clipboard.writeText(`https://whendiscount.com/hot-deals/${hotDeal.hotDealId}`);
+                    //     alert('클립보드에 복사되었습니다.');
+                    // } catch (error) {
+                    //     alert('복사에 실패하였습니다.');
+                    // }
+                    const $textarea = document.createElement('textarea');
+                    document.body.appendChild($textarea);
+                    // 2. props로 받은 text값을 textarea의 value로 대입하고 textarea 영역 내 모든 텍스트를 선택(드래그효과)
+                    $textarea.value = `https://whendiscount.com/hot-deals/${hotDeal.hotDealId}`;
+                    $textarea.select();
+                    // 3. execCommand 함수를 이용해 클립보드에 복사
+                    document.execCommand('copy');
+                    // 4. 임시 textarea 요소 제거
+                    document.body.removeChild($textarea);
+                    alert('클립보드에 복사되었습니다.');
                 }}>
                     <SvgIcon color={"primary"}>
                         <path  d="M10.59,13.41C11,13.8 11,14.44 10.59,14.83C10.2,15.22 9.56,15.22 9.17,14.83C7.22,12.88 7.22,9.71 9.17,7.76V7.76L12.71,4.22C14.66,2.27 17.83,2.27 19.78,4.22C21.73,6.17 21.73,9.34 19.78,11.29L18.29,12.78C18.3,11.96 18.17,11.14 17.89,10.36L18.36,9.88C19.54,8.71 19.54,6.81 18.36,5.64C17.19,4.46 15.29,4.46 14.12,5.64L10.59,9.17C9.41,10.34 9.41,12.24 10.59,13.41M13.41,9.17C13.8,8.78 14.44,8.78 14.83,9.17C16.78,11.12 16.78,14.29 14.83,16.24V16.24L11.29,19.78C9.34,21.73 6.17,21.73 4.22,19.78C2.27,17.83 2.27,14.66 4.22,12.71L5.71,11.22C5.7,12.04 5.83,12.86 6.11,13.65L5.64,14.12C4.46,15.29 4.46,17.19 5.64,18.36C6.81,19.54 8.71,19.54 9.88,18.36L13.41,14.83C14.59,13.66 14.59,11.76 13.41,10.59C13,10.2 13,9.56 13.41,9.17Z" />
