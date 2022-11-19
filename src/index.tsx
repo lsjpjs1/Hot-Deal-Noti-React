fc1ffcafe26ea03cf6db7453a46bc3a2a229bfdc
lsjpjs1 <lsjpjs1@naver.com>
2022-11-19 21:11:19 +0900

    fix : 디폴트 글씨체 라인으로 수정

--- src/index.tsx
+++ src/index.tsx
@@ -14,7 +14,8 @@ import ProductClassifyContainer from "./containers/ProductClassifyContainer";
 import ManageHotDealsContainer from "./containers/ManageHotDealsContainer";
 import AddHotDealContainer from "./containers/AddHotDealContainer";
 import RecommendationContainer from "./containers/RecommendationContainer";
-import ManageRecommendationContainer from "./containers/ManageRecommendationContainer"; // GA4
+import ManageRecommendationContainer from "./containers/ManageRecommendationContainer";
+import {createMuiTheme, MuiThemeProvider} from "@material-ui/core"; // GA4
 
 
 const TRACKING_ID = process.env["REACT_APP_GOOGLE_ANALYTICS_TRACKING_ID"]; // 발급받은 추적ID를 환경 변수로 불러온다.
@@ -25,23 +26,31 @@ const root = ReactDOM.createRoot(
 );
 export const store = createStore(rootReducer, applyMiddleware(thunk));
 
+const THEME = createMuiTheme({
+    typography: {
+        "fontFamily": "line"
+    }
+});
 root.render(
-  <Provider store={store}>
-      <BrowserRouter>
-          <Routes>
-              <Route path="/" element={<MainContainer/>}/>
-              <Route path='/hot-deals/:hotDealId' element={<MainContainer/>} />
-              <Route path='/hot-deals/product/:productId' element={<MainContainer/>} />
-              <Route path='/recommendation' element={<RecommendationContainer/>} />
-              <Route path='/hoon/recommendation' element={<ManageRecommendationContainer/>} />
-              <Route path="/hoon/980320" element={<ProductClassifyContainer/>}/>
-              <Route path="/hoon/980320/manage" element={<ManageHotDealsContainer/>}/>
-              <Route path="/hoon/980320/add" element={<AddHotDealContainer/>}/>
+    <MuiThemeProvider theme={THEME}>
+        <Provider store={store}>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<MainContainer/>}/>
+                    <Route path='/hot-deals/:hotDealId' element={<MainContainer/>} />
+                    <Route path='/hot-deals/product/:productId' element={<MainContainer/>} />
+                    <Route path='/recommendation' element={<RecommendationContainer/>} />
+                    <Route path='/hoon/recommendation' element={<ManageRecommendationContainer/>} />
+                    <Route path="/hoon/980320" element={<ProductClassifyContainer/>}/>
+                    <Route path="/hoon/980320/manage" element={<ManageHotDealsContainer/>}/>
+                    <Route path="/hoon/980320/add" element={<AddHotDealContainer/>}/>
 
-              {/*<Route path="/" element={<ServerMaintenanceContainer/>}/>*/}
-          </Routes>
-      </BrowserRouter>
-  </Provider>
+                    {/*<Route path="/" element={<ServerMaintenanceContainer/>}/>*/}
+                </Routes>
+            </BrowserRouter>
+        </Provider>
+    </MuiThemeProvider>
+
 );
 
 // If you want to start measuring performance in your app, pass a function
