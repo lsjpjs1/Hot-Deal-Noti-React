d487f3e9880d22816604be6196d8dbf1f106aab2
lsjpjs1 <lsjpjs1@naver.com>
2022-12-23 06:24:27 +0900

    feat: 쿠팡 파트너스 문구 추가

--- src/containers/MainContainer.tsx
+++ src/containers/MainContainer.tsx
@@ -246,7 +246,9 @@ const MainContainer = () => {
                 <PageView currentPage={getHotDealRequest.pageRequest.page} onPageChange={onPageChange}
                           totalPageCount={totalPages}></PageView>
                 <p><b>Email : whendiscount@gmail.com</b><br/>
-                    이용 중 불편사항이나 문의사항은 메일주시면 감사드리겠습니다!
+                    쿠팡 링크의 경우 쿠팡 파트너스 활동의 일환으로 이에 따른 일정액의 수수료를 제공받습니다.<br/>
+                    그러나 여러분이 구매하시는 가격엔 아무런 영향이 없습니다.<br/>
+                    이용 중 불편사항이나 문의사항은 메일주시면 감사드리겠습니다!<br/>
                 </p>
             </div>
         </div>
